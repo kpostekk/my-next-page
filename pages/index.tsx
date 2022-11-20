4218acbab607e1c27ab6a72d705c9cd254f8c343
Krystian Postek <krystian@postek.eu>
2022-11-20 20:52:27 +0100

    Minor style changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { TbBrandGithub, TbBrandLinkedin, TbDownload } from 'react-icons/tb'
 
 export default function Home() {
@@ -15,25 +13,21 @@ export default function Home() {
       <div className="w-screen h-screen relative overflow-clip">
         <img className="w-full h-full absolute object-cover" src="/cover.jpg" />
         <video className="w-full h-full absolute object-cover" autoPlay muted loop playsInline src="/waves.mp4" />
-        <div className="w-full h-full absolute">
-          <div className="w-full h-full flex flex-col justify-center p-4 md:p-36 items-start gap-2 select-none">
+        <div className="w-full h-full absolute z-40">
+          <div className="w-full h-full flex flex-col justify-center p-4 md:p-36 items-start gap-4 select-none">
             <div className="backdrop-blur bg-gray-800 bg-opacity-60 md:p-12 p-8 rounded-xl text-white grid gap-4">
               <h1 className="md:text-[44pt] text-[34pt] font-bold">@kpostekk</h1>
               <h2 className="italic">Krystian Postek - fullstack dev</h2>
-              <p className="select-text">JavaScript/TypeScript, Python, Docker, MongoDB, Postgres, GraphQL, Java</p>
+              <p className="select-text">JavaScript/TypeScript, Python, MongoDB, Postgres, GraphQL</p>
+              <a></a>
             </div>
             <div className="flex flex-col md:flex-row items-start md:items-stretch gap-2">
-              <div className="flex gap-6 items-center text-[24pt] backdrop-blur bg-gray-800 bg-opacity-60 p-6 rounded-xl text-white">
-                <a className="hover:text-sky-500" href="https://github.com/kpostekk">
+              <div className="flex gap-6 items-center text-[28pt] backdrop-blur bg-gray-800 bg-opacity-60 p-6 rounded-xl text-white">
+                <a href="https://github.com/kpostekk">
                   <TbBrandGithub />
                 </a>
-                <a className="hover:text-sky-500" href="https://www.linkedin.com/in/kpostekk/">
+                <a href="https://www.linkedin.com/in/kpostekk/">
                   <TbBrandLinkedin />
-                </a>
-              </div>
-              <div className="text-[18pt] backdrop-blur bg-gray-800 bg-opacity-60 p-6 rounded-xl text-white">
-                <a href="/cv.pdf" className="hover:text-sky-500">
-                  <p className="flex items-center gap-1 italic"><TbDownload /> curriculum vitae</p>
                 </a>
               </div>
             </div>
