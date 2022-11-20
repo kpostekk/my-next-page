f72559b7d95e3fcef5034b5523c4bbefb65747e7
Krystian Postek <krystian@postek.eu>
2022-11-20 16:30:02 +0100

    Fix view on mobile devices

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,27 +9,30 @@ export default function Home() {
       <Head>
         <title>@kpostekk</title>
         <meta name="description" content="My portfolio page" />
+        <meta name="theme-color" content="black" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
       </Head>
       <div className="w-screen h-screen relative overflow-clip">
         <img className="w-full h-full absolute object-cover" src="/cover.jpg" />
         <video className="w-full h-full absolute object-cover" autoPlay muted loop playsInline src="/waves.mp4" />
         <div className="w-full h-full absolute">
-          <div className="w-full h-full flex flex-col justify-center p-6 md:p-36 items-start gap-2 select-none">
-            <div className="backdrop-blur bg-gray-800 bg-opacity-60 p-12 rounded-xl text-white grid gap-4">
+          <div className="w-full h-full flex flex-col justify-center p-4 md:p-36 items-start gap-2 select-none">
+            <div className="backdrop-blur bg-gray-800 bg-opacity-60 md:p-12 p-8 rounded-xl text-white grid gap-4">
               <h1 className="md:text-[44pt] text-[34pt] font-bold">@kpostekk</h1>
               <h2 className="italic">Krystian Postek - fullstack dev</h2>
+              <p className="select-text">JavaScript/TypeScript, Python, Docker, MongoDB, Postgres, GraphQL, Java</p>
             </div>
             <div className="flex flex-col md:flex-row items-start md:items-stretch gap-2">
               <div className="flex gap-6 items-center text-[24pt] backdrop-blur bg-gray-800 bg-opacity-60 p-6 rounded-xl text-white">
-                <a className="hover:text-sky-600" href="https://github.com/kpostekk">
+                <a className="hover:text-sky-500" href="https://github.com/kpostekk">
                   <TbBrandGithub />
                 </a>
-                <a className="hover:text-sky-600" href="https://www.linkedin.com/in/kpostekk/">
+                <a className="hover:text-sky-500" href="https://www.linkedin.com/in/kpostekk/">
                   <TbBrandLinkedin />
                 </a>
               </div>
               <div className="text-[18pt] backdrop-blur bg-gray-800 bg-opacity-60 p-6 rounded-xl text-white">
-                <a href="/cv.pdf" className="hover:text-sky-600">
+                <a href="/cv.pdf" className="hover:text-sky-500">
                   <p className="flex items-center gap-1 italic"><TbDownload /> curriculum vitae</p>
                 </a>
               </div>
