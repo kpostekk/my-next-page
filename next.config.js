3d299f8c8ad61ccd1c7c205a0c61888ac5766459
Krystian Postek <krystian@postek.eu>
2022-11-20 16:11:07 +0100

    Minor changes for design, add CV

--- next.config.js
+++ next.config.js
@@ -2,14 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  pageExtensions: ['md', 'mdx', 'tsx', 'jsx']
+  pageExtensions: ['md', 'mdx', 'tsx', 'jsx'],
 }
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
-  options: {
-
-  }
 })
 
 module.exports = withMDX(nextConfig)
